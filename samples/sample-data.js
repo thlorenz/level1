e9b8e8a9b05c9565cc3b82db472b73a979c8dbcd
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-27 16:37:39 -0400

    extracing sample data

---
+++ samples/sample-data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.vehicles = {
+    car         :  [ 'drive', 'fast', 'dangerous', 'roof', 'engine' ]
+  , bus         :  [ 'drive', 'slow', 'roof', 'engine' ]
+  , bike        :  [ 'drive', 'fast', 'dangerous', 'engine' ]
+  , bicycle     :  [ 'drive', 'slow', 'dangerous' ]
+  , weelchair   :  [ 'drive', 'slow', 'dangerous' ]
+  , cane        :  [ 'walk', 'slow' ]
+  , hike        :  [ 'walk', 'slow' ]
+  , run         :  [ 'walk', 'fast' ]
+  , mountaineer :  [ 'walk', 'slow', 'dangerous' ]
+}
